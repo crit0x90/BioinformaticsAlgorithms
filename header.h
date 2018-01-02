4832c7880f4bd480363555e2487fd54651966cf8
scottmccoy13 <scottmccoy13@gmail.com>
2018-01-01 18:01:10 -0800

    commit to add the header file

---
+++ header.h
@@ -0,0 +1,19 @@
+//This file will serve as the main header file for the program
+
+//exhaustive search algorithms
+
+//greedy algorithms
+
+//dynamic programming algorithms
+
+//divide and conquer algorithms
+
+//graph algorithms
+
+//combinatorial pattern matching
+
+//clustering and trees
+
+//hidden markov models
+
+//randomized algorithms
