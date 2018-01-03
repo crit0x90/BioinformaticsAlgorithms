39788e2d2fcbd82d2028db3548961362c577b97e
scottmccoy13 <scottmccoy13@gmail.com>
2018-01-02 20:13:55 -0800

    more code added to the brute force PDP algorithm, nearly finished now.

--- driver.cpp
+++ driver.cpp
@@ -6,7 +6,6 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello world" << endl;
 	vector<int> a = {0,2,4,7,10}; //set of points
 	int n = a.size(); 
 
--- exhaustiveSearch.cpp
+++ exhaustiveSearch.cpp
@@ -3,20 +3,56 @@
 #include "header.h"
 using namespace std;
 
+
+vector<int> delta(vector<int>& x)
+{
+	vector<int> delta;
+	int j = x.size() - 1;
+
+	cout << "X" << endl;
+	for(int i = 0; i < j; i++)
+	{
+		cout << x.at(i) << " ";
+	}
+	cout << endl;
+	
+	while(j > 0)
+	{
+		for(int i = 0; i < j; i++)
+		{
+			cout << "i: " << i << " j: " << j << endl;
+			delta.push_back((x.at(j) - x.at(i)));
+		}
+
+		j--;
+	}
+
+	cout << "Delta X" << endl;
+	for(int i = 0; i < delta.size(); i++)
+	{
+		cout << delta.at(i) << ", ";
+	}
+
+	//merge sort of delta x
+	
+	return delta;
+}
+
 //takes roughly O(M^n-2) time
-void bruteForcePDP(vector<int>& a, int n)
+void bruteForcePDP(vector<int>& x, int n)
 {
 	int m = 0;
 
 	//get max elt in vector for m
-	for(int i = 0; i < a.size(); i++)
+	for(int i = 0; i < x.size(); i++)
 	{
-		if(a.at(i) > m)
+		if(x.at(i) > m)
 		{
-			m = a.at(i);
+			m = x.at(i);
 		}
 	}
 
-
+	//delta creates the pairwise distance of all points in multiset X
+	vector<int> delta_x = delta(x); 
  
 }
